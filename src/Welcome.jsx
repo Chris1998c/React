953317bb56e91d00aa1918da1c1bd05ca15b5914
Chris1998c <contechristian.98@gmail.com>
2023-10-07 13:45:36 +0200

    Exercise24

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,13 +2,13 @@ import Age from "./age"
 
 
 
-function Welcome(props){
-return (
-    <div>
-    <p><strong>Welcome, {props.name}.</strong></p>
-    {props.age >=18 && props.age <=65 && <Age age={props.age}/>}
-    </div>
-);
+function Welcome(props) {
+    return (
+        <div>
+            <p><strong>Welcome, {props.name}.</strong></p>
+            {(props.age > 18 && props.age < 65 && props.name === "John") && <Age age={props.age} />}
+        </div>
+    );
 }
 
 export default Welcome
