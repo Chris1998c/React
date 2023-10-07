b6a5a89ea574dd9cdfe7a4ac61ee1dd07009a02b
Chris1998c <contechristian.98@gmail.com>
2023-10-07 13:49:47 +0200

    Exercise25

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -6,7 +6,9 @@ function Welcome(props) {
     return (
         <div>
             <p><strong>Welcome, {props.name}.</strong></p>
-            {(props.age > 18 && props.age < 65 && props.name === "John") && <Age age={props.age} />}
+            {(props.age > 18)
+                ? <Age age={props.age} />
+                : <p><strong>You are very young!</strong></p>}
         </div>
     );
 }
