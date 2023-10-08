ed301e2a14151b501f7d82ab346eef320afb2b46
Chris1998c <contechristian.98@gmail.com>
2023-10-08 18:26:26 +0200

    Exercise38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
-function MouseClicker({name}){
-const handleClick= (event)=>{
-    if (event.target.nodeName === 'BUTTON' || event.target.nodeName === 'IMG') {
-        console.log(event.currentTarget.getAttribute('name'));
-      }
+function MouseClicker({ name }) {
+    const handleButtonClick = (event) => {
+      console.log(event.currentTarget.getAttribute('name'));
     };
-return(
-      <button name={name} onClick={handleClick}>
-        <img src="https://images.photowall.com/products/63176/lower-manhattan-skyline.jpg?h=699&q=85" alt="IMMAGINE1" />
+  
+    const handleImageClick = (event) => {
+      console.log(event.target.src);
+      event.stopPropagation(); // Impedisce la propagazione dell'evento verso l'alto fino al pulsante
+    };
+
+return (
+    <div>
+      <button name={name} onClick={handleButtonClick}>
+        <img src="https://images.photowall.com/products/63176/lower-manhattan-skyline.jpg?h=699&q=85" alt="IMMAGINE1" 
+        onClick={handleImageClick}/>
         Click
-      </button>
-)
+        </button>
+    </div>
+  );
 }
 
 export default MouseClicker
