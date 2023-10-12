15be6d2e0f60b7aecab5e1825e41d32d06bcdf8d
Chris1998c <contechristian.98@gmail.com>
2023-10-12 17:00:44 +0200

    Exercise45

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,50 +2,54 @@
 
 import { useState } from "react";
 
-
 export function Login({ onLogin }) {
     const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("")
-    const [remember, setRemember] = useState("");
 
+    const [password, setPassword] = useState("");
+
+    const [remember, setRemember] = useState(""); 
+  
     const handleUsername = (event) => {
-        setUsername(event.target.value)
-
+      setUsername(event.target.value);
     };
-
+  
     const handlePassword = (event) => {
-        setPassword(event.target.value)
-    }
-
+      setPassword(event.target.value);
+    };
+  
     const handleRememberChange = () => {
-        setRemember(!remember);
+      setRemember(!remember); 
+    };
+  
+    const handleLogin = (event) => {
+        console.log(username, password, remember)
+        event.preventDefault()
+
+      };
+     
+    const handleReset = () => {
+      setUsername("");
+      setPassword("");
+      setRemember(""); 
     };
 
-    const handleLogin = () => {
-        onLogin({ username, password, remember });
-    };
 
-    const handleReset=()=>{
-        setUsername("");
-        setPassword("");
-        setRemember("");
-    }
+     const loginDisable = username === "" && password === ""; 
 
-     const loginDisable = username === "" && password === ""
-
-
-    return (
+  return (
     <div>
-            <input type="text" placeholder="Username" value={username} onChange={handleUsername}
-            />
+      <form onSubmit={handleLogin}>
+        <input name="username" type="text" placeholder="Username" value={username} onChange={handleUsername} />
 
-            <input type="password" placeholder="Password" value={password} onChange={handlePassword} />
+        <input name="password" type="password" placeholder="Password" value={password} onChange={handlePassword} />
 
-            <input type="checkbox" checked="remeber" onChange={handleRememberChange} />
+        <input name="check" type="checkbox" checked={remember} onChange={handleRememberChange} />
+        <button  type="button" onClick={handleReset}>Reset</button>
 
-            <button onClick={handleLogin} disabled={loginDisable} type="submit">Login</button>
-            <button onClick={handleReset}>reset</button>
-        </div>
-    )
+        <button className="bottone" type="submit" disabled={loginDisable}>Login</button>
+
+
+      </form>
+    </div>
+  );
 }
-
