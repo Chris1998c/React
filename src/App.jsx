5bf7d1e46df7d19210615da7b69b35e38af0aec8
Chris1998c <contechristian.98@gmail.com>
2023-10-19 10:48:11 +0200

    Update Exercise14

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 
-
-function Message(){
-    return(
-        <div>
-        <h2>Hello,World! </h2>
-
-<p>"What a beautiful day!".</p>
-</div>
-)}
+import { Message } from "./Message";
 
 
 function App(){
   return(
     <div>
-      <Message/>
       <Message/>
       <Message/>
     </div>
---
+++ src/Message.jsx
@@ -0,0 +1,8 @@
+export function Message(){
+    return(
+        <div>
+        <h2>Hello,World! </h2>
+
+<p>"What a beautiful day!".</p>
+</div>
+)}
