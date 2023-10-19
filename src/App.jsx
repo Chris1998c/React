582b0342a2e8a6a12f6e06d547b642fb798c33d7
Chris1998c <138510785+Chris1998c@users.noreply.github.com>
2023-10-19 12:14:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-
 import Welcome from "./Welcome";
-
-
-
 
 function App(){
   return(
     <div>
-  <Welcome name={<p><strong>"Christian"</strong></p>}    age= {24} />
-  
+  <Welcome name={<p><strong>"Christian"</strong></p>}  age= {24} />  
     </div>
   )
 }
