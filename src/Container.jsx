c66c26e2527d5b1a4c9df0bacf067f663ee6602a
Chris1998c <contechristian.98@gmail.com>
2023-10-21 18:58:29 +0200

    Exercise69

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function Container({ children, title }) {
+    const [collapsed, setCollapsed] = useState(false)
+
+    const handleCollapsed = () => {
+        setCollapsed(!collapsed)
+    }
     return (
         <div className="container">
-            {<h2>{title}</h2>}
-            {children}
+            {<button onClick={handleCollapsed}><h2>{title}</h2></button>}
+            {collapsed ? null : children}
         </div>
     );
 }
