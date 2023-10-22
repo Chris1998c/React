dc2d0e21c29e5d64719af16966048335a338bede
Chris1998c <contechristian.98@gmail.com>
2023-10-22 14:03:24 +0200

    exercise72

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { LanguageProvider } from './LanguageContext';
 import { Clock } from './Clock';
 
+ // Se provo a rimuovere  il Provider del Context cioè LanguageProvider che avvolge il componente Clock, il componente Clock perderà l'accesso al LanguageContext, e il codice smetterà di funzionare.
+// Nel componente Clock, l' useContext cerca di accedere al LanguageContext per selezionare la lingua  e visualizzare il messaggio nella lingua corretta. Se rimuovi il LanguageProvider, il LanguageContext non fuonzionerà più.
+
 function App(){
   return (
-    <LanguageProvider>
       <div>
         <h1>Language Selector</h1>
         <Clock/>
       </div>
-    </LanguageProvider>
   );
 };
-
 export default App;
