d7dddd3533fd12ef4a4cf0274c07b2c00644e108
Chris1998c <contechristian.98@gmail.com>
2023-10-22 14:40:04 +0200

    Update Exercise73

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { LanguageProvider } from './LanguageContext';
-import { Clock } from './Clock';
+import { GithubUser } from './GithubUser';
 
- // Se provo a rimuovere  il Provider del Context cioè LanguageProvider che avvolge il componente Clock, il componente Clock perderà l'accesso al LanguageContext, e il codice smetterà di funzionare.
-// Nel componente Clock, l' useContext cerca di accedere al LanguageContext per selezionare la lingua  e visualizzare il messaggio nella lingua corretta. Se rimuovi il LanguageProvider, il LanguageContext non fuonzionerà più.
 
 function App(){
   return (
       <div>
-        <h1>Language Selector</h1>
-        <Clock/>
+        <GithubUser username="Chris1998c"/>
       </div>
   );
 };
