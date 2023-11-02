d0b9957e8a8d6343bcd1e974d7f53e5770ac48a1
Chris1998c <contechristian.98@gmail.com>
2023-11-02 19:10:57 +0100

    Exercise91

--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -4,7 +4,11 @@ const fetcher = (url) => fetch(url).then((response) => response.json())
 
 export function useGithubUser(){
     
-    const { data, error } = useSWR(`https://api.github.com/users`, fetcher)
+    const { data, error, mutate } = useSWR(`https://api.github.com/users`, fetcher)
 
-    return { user: data, error, isLoading: !data && !error }
+    function handleRefresh(){
+        mutate()
+    }
+
+    return { user: data, error, isLoading: !data && !error, onRefresh: handleRefresh }
 }
