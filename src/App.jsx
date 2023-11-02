e8d704d91c66bcce454087263eafd2c396544996
Chris1998c <contechristian.98@gmail.com>
2023-11-02 18:10:08 +0100

    Exercise85

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
+import { Counter } from "./Counter"
 import { ShowGithubUser } from "./ShowGithubUser"
-import { Counter } from "./Counter.Jsx"
 import { Welcome } from "./Welcome"
 
 function App() {
 
   return (
+    <div>
+
+      <div>
+        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="users/Chris1998c">Github User</Link>
+      </div>
+    
     <Routes>
       <Route path="/" element={<Welcome name="Christian"/>}/>
       <Route path="/counter" element={<Counter initialValue={0} incrementAmount={1}/>}/>
       <Route path="users/:username" element={<ShowGithubUser/>}/>
     </Routes>
+    </div>
   )
 }
 
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,8 +1,7 @@
-export function Welcome() {
-    return (
-        <div className="welcome">
-            <h1>Welcome to My App</h1>
-            <p>This is the welcome message.</p>
+export function Welcome({name}){
+    return(
+        <div>
+            <h1>Welcome, {name}</h1>
         </div>
     )
 }
